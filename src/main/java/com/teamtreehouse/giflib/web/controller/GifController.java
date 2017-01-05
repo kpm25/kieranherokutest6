c35b2518b96ba0fea89e163b2039c68645f5bc28
kpm25 <kieran928@gmail.com>
2017-01-05 19:44:17 +1300

    test with javascript and thymeleaf

--- src/main/java/com/teamtreehouse/giflib/web/controller/GifController.java
+++ src/main/java/com/teamtreehouse/giflib/web/controller/GifController.java
@@ -155,4 +155,36 @@ public class GifController {
         model.addAttribute("gifs",gifs);
         return "gif/index";
     }
+    
+    //////////////////////////////////////
+    
+    @RequestMapping(path = "/testgif", method = RequestMethod.GET)
+    public String testTask2(Model model) {
+    	  // TODO: Get all gifs
+ 
+    	 Gif gif1 = gifService.findById( (long) 1);
+    	 Gif gif2 = gifService.findById((long) 2);
+    	 Gif gif3 = gifService.findById((long) 3);
+    	 Gif gif33 = gifService.findById((long) 33);
+    	 Gif gif34 = gifService.findById((long) 34);
+     	
+    	 Gif gif65 = gifService.findById((long) 65);
+     	
+    	 Gif gif66 = gifService.findById((long) 66);
+     	
+    	
+
+        model.addAttribute("gif1", gif1);
+        model.addAttribute("gif2", gif2);
+        model.addAttribute("gif3", gif3);
+        model.addAttribute("gif33", gif33);
+        model.addAttribute("gif34", gif34);
+        model.addAttribute("gif65", gif65);
+        model.addAttribute("gif66", gif66);
+       
+        
+       
+    	
+        return "gif/testgif";
+    }
 }

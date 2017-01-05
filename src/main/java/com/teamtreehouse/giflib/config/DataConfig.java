643ddb4ac9fc8cd70ff5cbc259915f1f24f26202
kpm25 <kieran928@gmail.com>
2017-01-06 05:04:59 +1300

    ssss

--- src/main/java/com/teamtreehouse/giflib/config/DataConfig.java
+++ src/main/java/com/teamtreehouse/giflib/config/DataConfig.java
@@ -10,6 +10,8 @@ import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
 
+import java.util.Properties;
+
 import javax.sql.DataSource;
 
 @Configuration
@@ -20,13 +22,18 @@ public class DataConfig {
 
     @Bean
     public LocalSessionFactoryBean sessionFactory() {
-        Resource config = new ClassPathResource("hibernate.cfg.xml");
+   //     Resource config = new ClassPathResource("hibernate.cfg.xml");
         LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
-        sessionFactory.setConfigLocation(config);
+    //    sessionFactory.setConfigLocation(config);
+        sessionFactory.setHibernateProperties(getHibernateProperties() );
+        
         sessionFactory.setPackagesToScan(env.getProperty("giflib.entity.package"));
         sessionFactory.setDataSource(dataSource());
+       
         return sessionFactory;
     }
+    
+   // factory.setJpaProperties(getHibernateProperties());
 
     @Bean
     public DataSource dataSource() {
@@ -43,5 +50,15 @@ public class DataConfig {
         ds.setPassword(env.getProperty("giflib.db.password"));
 
         return ds;
+    }
+    
+    private Properties getHibernateProperties() {
+        Properties properties = new Properties();
+        properties.put("hibernate.dialect", env.getProperty("hibernate.dialect"));
+        properties.put("hibernate.implicit_naming_strategy",env.getProperty("hibernate.implicit_naming_strategy"));
+        properties.put("hibernate.format_sql", env.getProperty("hibernate.format_sql"));
+        properties.put("hibernate.show_sql", env.getProperty("hibernate.show_sql"));
+        properties.put("hibernate.hbm2ddl.auto", env.getProperty("hibernate.hbm2ddl.auto"));
+        return properties;
     }
 }
